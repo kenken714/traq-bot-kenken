272321e612ae95b9392d5461f1d54152fe67fe74
kenken714 <kenkenkeiko043@gmail.com>
2024-10-30 20:59:02 +0900

    feat:impl debug for error

--- game/akinator/src/error.rs
+++ game/akinator/src/error.rs
@@ -1,3 +1,5 @@
+use std::fmt::Debug;
+
 pub enum AkinatorError {
     SessionNotFound,
     ConnectionError,
@@ -5,4 +7,17 @@ pub enum AkinatorError {
     InvalidResponse,
     UnexpectedError,
     CannotBackAnyMore,
+}
+
+impl Debug for AkinatorError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            AkinatorError::SessionNotFound => write!(f, "Session not found"),
+            AkinatorError::ConnectionError => write!(f, "Connection error"),
+            AkinatorError::QuestionLimitExceeded => write!(f, "Question limit exceeded"),
+            AkinatorError::InvalidResponse => write!(f, "Invalid response"),
+            AkinatorError::UnexpectedError => write!(f, "Unexpected error"),
+            AkinatorError::CannotBackAnyMore => write!(f, "Cannot back any more"),
+        }
+    }
 }
